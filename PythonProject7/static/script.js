1c043551a331ce625734222be293d0f7bb251d41
akauntsuper04-cyber <akauntsuper04@gmail.com>
2026-02-22 19:22:49 +0100

    Delete PythonProject7 directory

--- PythonProject7/static/script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll("a").forEach(anchor => {
-    anchor.addEventListener("click", function(e) {
-        if(this.hash !== "") {
-            e.preventDefault();
-            document.querySelector(this.hash)
-                .scrollIntoView({ behavior: "smooth" });
-        }
-    });
-});
