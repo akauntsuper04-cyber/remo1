99aa7f70f41acb8b11a6839f90e5f9009866b2d3
akauntsuper04-cyber <akauntsuper04@gmail.com>
2026-02-22 19:38:02 +0100

    first commit

---
+++ static/script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll("a").forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        if(this.hash !== "") {
+            e.preventDefault();
+            document.querySelector(this.hash)
+                .scrollIntoView({ behavior: "smooth" });
+        }
+    });
+});
