21702653e59c883001692f18fa75866f773075aa
akauntsuper04-cyber <akauntsuper04@gmail.com>
2026-03-01 18:10:12 +0100

    add match pages with full stats

--- main.py
+++ main.py
@@ -6,13 +6,25 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/roster")
+def roster():
+    return render_template("roster.html")
+
 @app.route("/matches")
 def matches():
     return render_template("matches.html")
+
+@app.route("/match1")
+def match1():
+    return render_template("match1.html")
+
+@app.route("/match2")
+def match2():
+    return render_template("match2.html")
 
 @app.route("/rules")
 def rules():
     return render_template("rules.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
